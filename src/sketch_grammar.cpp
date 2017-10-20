241f3c3665fbfa297cfa4f21d1a45d719c397aca
Vladislav Markov <vmarkov@cranems.com>
2017-10-21 00:47:58 +0300

    position now can hold % / px / centered

--- src/sketch_grammar.cpp
+++ src/sketch_grammar.cpp
@@ -12,28 +12,28 @@
 namespace sk {
 
 namespace ast {
-using percent_t = double;
-using pixels_t  = std::size_t;
+using percent_t = double;      // in 1/100ths
+using pixels_t  = std::size_t; // self-explanatory
 
-/* window width type can be unspecified or specified in percent or specified
- * in pixels
+/* window width type can be unspecified or specified to be screen-wide, or
+ * specified in percent or specified in pixels
  */
-using width_t = std::optional<std::variant<percent_t, pixels_t>>;
+using width_t = std::optional<std::variant<bool, percent_t, pixels_t>>;
 
-/* window height type can be unspecified or specified in percent or
- * specified in pixels
+/* window height type can be unspecified or specified to be screen-high, or
+ * specified in percent or specified in pixels
  */
-using height_t = std::optional<std::variant<percent_t, pixels_t, bool>>;
+using height_t = std::optional<std::variant<bool, percent_t, pixels_t>>;
 
-/* horizontal window coordinates can be represented by pixels or window can
- * be simply centered horizontally
+/* horizontal window coordinates can be represented by pixels or by percents of
+ * screen, or window can be simply centered horizontally
  */
-using horizontal_t = std::variant<bool, pixels_t>;
+using horizontal_t = std::variant<bool, percent_t, pixels_t>;
 
-/* vertical window coordinates can be represented by pixels or window can be
- * simply centered vertically
+/* vertical window coordinates can be represented by pixels or by percents of
+ * screen, or window can be simply centered vertically
  */
-using vertical_t = std::variant<bool, pixels_t>;
+using vertical_t = std::variant<bool, percent_t, pixels_t>;
 
 // point for window is represented by a pair of window coordinates
 using point_t = std::pair<horizontal_t, vertical_t>;
@@ -54,51 +54,51 @@ class window_ast {
     ast::position_t position;
 
 public:
-    void
+    bool
     set_title(const std::string& t)
     {
         if (!title.empty()) {
             std::cerr << "title is already set" << std::endl;
-            return;
+            return false;
         }
-
         title = t;
+        return true;
     }
 
     template <typename Width>
-    void
+    bool
     set_width(const Width& p)
     {
         if (width) {
             std::cerr << "width is already set" << std::endl;
-            return;
+            return false;
         }
-
         width = p;
+        return true;
     }
 
     template <typename Height>
-    void
+    bool
     set_height(const Height& p)
     {
         if (height) {
             std::cerr << "height is already set" << std::endl;
-            return;
+            return false;
         }
-
         height = p;
+        return true;
     }
 
     template <typename Position>
-    void
+    bool
     set_position(const Position& p)
     {
         if (position) {
             std::cerr << "position is already set" << std::endl;
-            return;
+            return false;
         }
-
         position = p;
+        return true;
     }
 
     void
@@ -107,8 +107,8 @@ public:
         std::string width_str = "unspecified";
         if (width) {
             if (std::holds_alternative<ast::percent_t>(*width)) {
-                auto width_percent = std::get<ast::percent_t>(*width);
-                width_str          = std::to_string(
+                const auto width_percent = std::get<ast::percent_t>(*width);
+                width_str                = std::to_string(
                                 static_cast<std::size_t>(width_percent * 100)) +
                             "%";
             } else {
@@ -120,7 +120,7 @@ public:
         std::string height_str = "unspecified";
         if (height) {
             if (std::holds_alternative<ast::percent_t>(*height)) {
-                auto height_percent = std::get<ast::percent_t>(*height);
+                const auto height_percent = std::get<ast::percent_t>(*height);
                 height_str =
                     std::to_string(
                         static_cast<std::size_t>(height_percent * 100)) +
@@ -136,27 +136,45 @@ public:
             if (std::holds_alternative<bool>(*position)) {
                 position_str = "centered";
             } else {
-                auto pos = std::get<ast::point_t>(*position);
+                const auto pos = std::get<ast::point_t>(*position);
                 if (std::holds_alternative<bool>(pos.first) &&
                     std::holds_alternative<bool>(pos.second)) {
                     position_str = "centered";
                 } else {
                     position_str = "position = { ";
                     if (std::holds_alternative<bool>(pos.first)) {
-                        position_str += "h-centered, ";
+                        position_str += "h-centered";
+                    } else if (
+                        std::holds_alternative<ast::percent_t>(pos.first)) {
+                        const auto h_percent =
+                            std::get<ast::percent_t>(pos.first);
+                        position_str +=
+                            std::to_string(
+                                static_cast<std::size_t>(h_percent * 100)) +
+                            "%";
                     } else {
                         position_str +=
                             std::to_string(std::get<ast::pixels_t>(pos.first));
-                        position_str += ", ";
                     }
+
+                    position_str += ", ";
 
                     if (std::holds_alternative<bool>(pos.second)) {
                         position_str += "v-centered }";
+                    } else if (
+                        std::holds_alternative<ast::percent_t>(pos.second)) {
+                        const auto v_percent =
+                            std::get<ast::percent_t>(pos.second);
+                        position_str +=
+                            std::to_string(
+                                static_cast<std::size_t>(v_percent * 100)) +
+                            "%";
                     } else {
                         position_str +=
                             std::to_string(std::get<ast::pixels_t>(pos.second));
-                        position_str += " }";
                     }
+
+                    position_str += " }";
                 }
             }
         }
@@ -195,75 +213,68 @@ struct error_handler_base {
     }
 };
 
-struct annotation_base {
-    template <typename T, typename Iterator, typename Context>
-    void
-    on_success(
-        Iterator const& first,
-        Iterator const& last,
-        T&              ast,
-        Context const&  context)
-    {
-        auto& error_handler = x3::get<error_handler_tag>(context).get();
-        error_handler.tag(ast, first, last);
-    }
-};
-
 struct window_class;
 
-const auto title = x3::rule<struct title, std::string>{"title"};
-
+const auto title     = x3::rule<struct title, std::string>{"title"};
 const auto title_def = x3::lexeme['"' >> +(x3::char_ - '"') >> '"'];
 
-const auto percent = x3::rule<struct percent, ast::percent_t>{"percent"};
+const auto centered = x3::rule<struct centered, bool>{"centered"};
+const auto centered_def =
+    x3::lit("centered")[([](auto& ctx) { x3::_val(ctx) = true; })];
 
+const auto percent = x3::rule<struct percent, ast::percent_t>{"percent"};
 const auto percent_def =
-    (+x3::digit)[([](auto& ctx) {
-        x3::_val(ctx) = static_cast<double>(
-                            std::strtoul(x3::_attr(ctx).c_str(), nullptr, 0)) /
-                        100.0;
-    })] > '%';
+    (x3::uint_[([](auto& ctx) {
+         x3::_val(ctx) = static_cast<ast::percent_t>(x3::_attr(ctx)) / 100.0;
+     })] > '%');
+
+const auto pixels = x3::rule<struct pixels, ast::pixels_t>{"pixels"};
+const auto
+    pixels_def = x3::uint_[([](auto& ctx) {
+                     x3::_val(ctx) = static_cast<ast::pixels_t>(x3::_attr(ctx));
+                 })] > x3::lit("px");
 
 const auto width = x3::rule<struct width, ast::width_t>{"width"};
-
-const auto width_def = x3::lit("width") > '=' > percent[([](auto& ctx) {
-                           try {
-                               x3::_val(ctx) = x3::_attr(ctx);
-                           } catch (...) {
-                               std::cerr << "width";
-                           }
-                       })];
+const auto width_def =
+    x3::lit("width") > '=' >
+    percent[([](auto& ctx) { x3::_val(ctx) = x3::_attr(ctx); })];
 
 const auto height = x3::rule<struct height, ast::height_t>{"height"};
-
 const auto height_def =
     x3::lit("height") > '=' >
     percent[([](auto& ctx) { x3::_val(ctx) = x3::_attr(ctx); })];
 
-const auto centered = x3::rule<struct centered, bool>{"centered"};
+const auto horizontal =
+    x3::rule<struct horizontal, ast::horizontal_t>{"horizontal"};
+const auto horizontal_def =
+    centered[([](auto& ctx) { x3::_val(ctx) = x3::_attr(ctx); })] |
+    percent[([](auto& ctx) { x3::_val(ctx) = x3::_attr(ctx); })] |
+    pixels[([](auto& ctx) { x3::_val(ctx) = x3::_attr(ctx); })];
 
-const auto centered_def =
-    x3::lit("centered")[([](auto& ctx) { x3::_val(ctx) = true; })];
+const auto vertical = x3::rule<struct vertical, ast::vertical_t>{"vertical"};
+const auto vertical_def =
+    centered[([](auto& ctx) { x3::_val(ctx) = x3::_attr(ctx); })] |
+    percent[([](auto& ctx) { x3::_val(ctx) = x3::_attr(ctx); })] |
+    pixels[([](auto& ctx) { x3::_val(ctx) = x3::_attr(ctx); })];
+
+const auto point = x3::rule<struct point, ast::point_t>{"point"};
+const auto point_def =
+    x3::lit("position") > '=' >
+    horizontal[([](auto& ctx) { x3::_val(ctx).first = x3::_attr(ctx); })] >
+    ',' > vertical[([](auto& ctx) { x3::_val(ctx).second = x3::_attr(ctx); })];
+
+const auto position = x3::rule<struct position, ast::position_t>{"position"};
+const auto position_def = centered | point;
 
 const auto attribute =
     x3::rule<struct attribute,
-             std::variant<ast::width_t, ast::height_t, ast::position_t>>{
+             std::tuple<ast::width_t, ast::height_t, ast::position_t>>{
         "attribute"};
 
 const auto attribute_def =
-    width[([](auto& ctx) {
-        try {
-            std::get<ast::width_t>(x3::_val(ctx)) = x3::_attr(ctx);
-        } catch (...) {
-            std::cout << "oops";
-        }
-    })] |
-    height[([](auto& ctx) {
-        std::get<ast::height_t>(x3::_val(ctx)) = x3::_attr(ctx);
-    })] |
-    centered[([](auto& ctx) {
-        std::get<ast::position_t>(x3::_val(ctx)) = x3::_attr(ctx);
-    })];
+    width[([](auto& ctx) { std::get<0>(x3::_val(ctx)) = x3::_attr(ctx); })] |
+    height[([](auto& ctx) { std::get<1>(x3::_val(ctx)) = x3::_attr(ctx); })] |
+    position[([](auto& ctx) { std::get<2>(x3::_val(ctx)) = x3::_attr(ctx); })];
 
 const auto window = x3::rule<struct window_class, window_ast>{"window"};
 
@@ -271,25 +282,37 @@ const auto window_def =
     x3::lit("window") > '=' >
     title[([](auto& ctx) { x3::_val(ctx).set_title(x3::_attr(ctx)); })] > ':' >
     +attribute[([](auto& ctx) {
-        try {
-            if (std::holds_alternative<ast::width_t>(x3::_attr(ctx))) {
-                x3::_val(ctx).set_width(std::get<ast::width_t>(x3::_attr(ctx)));
+        if (std::get<0>(x3::_attr(ctx))) {
+            if (!x3::_val(ctx).set_width(std::get<0>(x3::_attr(ctx)))) {
+                x3::_pass(ctx) = false;
+            };
+        }
+        if (std::get<1>(x3::_attr(ctx))) {
+            if (!x3::_val(ctx).set_height(std::get<1>(x3::_attr(ctx)))) {
+                x3::_pass(ctx) = false;
             }
-            if (std::holds_alternative<ast::height_t>(x3::_attr(ctx))) {
-                x3::_val(ctx).set_height(
-                    std::get<ast::height_t>(x3::_attr(ctx)));
+        }
+        if (std::get<2>(x3::_attr(ctx))) {
+            if (!x3::_val(ctx).set_position(std::get<2>(x3::_attr(ctx)))) {
+                x3::_pass(ctx) = false;
             }
-            if (std::holds_alternative<ast::position_t>(x3::_attr(ctx))) {
-                x3::_val(ctx).set_position(
-                    std::get<ast::position_t>(x3::_attr(ctx)));
-            }
-        } catch (...) {
-            std::cerr << "oops";
         }
     })];
 
 // window parsing rules
-BOOST_SPIRIT_DEFINE(attribute, centered, height, percent, title, width, window)
+BOOST_SPIRIT_DEFINE(
+    attribute,
+    centered,
+    height,
+    horizontal,
+    percent,
+    pixels,
+    point,
+    position,
+    title,
+    vertical,
+    width,
+    window)
 
 struct window_class {
     // error handler
